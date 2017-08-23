bfc1eeb7587916e0e0da9be23955c46d0a820532
Josefina Terrera <josefina.terrera@designory.com>
2017-08-23 13:25:50 -0500

    updates on the search page

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class BooksApp extends React.Component {
     componentDidMount() {
     API.getAll().then(books => {
       const bookShelves = this.sortBooks(books);
-      const filteredBooks = books;
+      const filteredBooks = [];
       this.setState({books, bookShelves, filteredBooks})
     });
   }
@@ -33,7 +33,6 @@ class BooksApp extends React.Component {
     //   book.title.toLowerCase()),filter(b => b.indexOf(userInput) >= 0 
     // )
     // console.log(filterBooks);
-
     //also tried it with reduce:
     const reducer = input => (acc, bookObj) => {
       return bookObj.title.toLowerCase().indexOf(input) >= 0 
@@ -43,6 +42,10 @@ class BooksApp extends React.Component {
     const filteredBooks = (this.state.books.reduce(reducer(userInput), []))
     // console.log(filteredBooks);
     this.setState({filteredBooks})
+     if (!inputValue) {
+        this.setState({ filteredBooks: []})
+     }
+    
 
   }
 
